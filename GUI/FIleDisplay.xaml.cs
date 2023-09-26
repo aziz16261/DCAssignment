a4a8313def1dfa0373c71dd4999da13f4e9f79d1
Shaky-Heart <cvstirling5@gmail.com>
2023-09-26 23:01:23 +0800

    File upload and download works

--- GUI/FIleDisplay.xaml.cs
+++ GUI/FIleDisplay.xaml.cs
@@ -2,6 +2,7 @@
 using DatabaseLib;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,9 +12,11 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace GUI
 {
@@ -39,7 +42,16 @@ namespace GUI
         {
             string selectedFile = FileSelectionBox.SelectedItem.ToString();
 
-            byte[] fileData = File.ReadAllBytes(selectedFile);
+            if (Path.GetExtension(selectedFile) == ".txt")
+            {
+                TextFileDisplay.Text = File.ReadAllText("..\\..\\..\\ConsoleApp1\\bin\\Debug\\" + selectedFile);
+            } else
+            {
+                string filePath = "..\\..\\..\\ConsoleApp1\\bin\\Debug\\" + selectedFile;
+                Bitmap image = new Bitmap(filePath);
+                var imageHandle = image.GetHbitmap();
+                ImageFileDisplay.Source = Imaging.CreateBitmapSourceFromHBitmap(imageHandle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
         }
     }
 }
