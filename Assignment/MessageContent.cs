eeb0a8bc13f8005ed373aed48f948bf65da06972
Rafe <raferaed98@gmail.com>
2023-09-27 13:22:52 +0800

    Changed names to suitable ones

--- Assignment/MessageContent.cs
+++ Assignment/MessageContent.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Assignment
 {
-    // This is for the file sharing later on, need to sort it out
     public class MessageContent
     {
         public string Text { get; set; }
